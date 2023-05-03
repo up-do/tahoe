ae94d2b83893834a55f304639c0b675c02df9c7e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-05-03 19:46:13 -0400

    make the client test usable

--- client-test/Main.hs
+++ client-test/Main.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
-
--- import Data.Aeson hiding
 
 import Codec.CBOR.Encoding
 import Codec.CBOR.FlatTerm
@@ -12,51 +10,80 @@ import Codec.CBOR.Pretty
 import Codec.Serialise
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import Data.ByteString.Base32 (encodeBase32Unpadded)
+import Data.ByteString.Base64 (encodeBase64)
 import qualified Data.ByteString.Lazy as BSL
 import Data.Map
 import Data.Proxy
 import qualified Data.Set as Set
 import Data.Text
+import Data.Text.Encoding
 import GHC.Generics
 import Network.Connection
 import Network.HTTP.Client hiding (Proxy)
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types
+import Network.URI
 import Servant.API
 import Servant.Client
 import qualified Servant.Client.Streaming as S
 import Servant.Types.SourceT
+import System.Environment (getArgs)
 import Tahoe.CHK.Capability
+import Tahoe.CHK.Share (getVersion)
 import TahoeLAFS.Storage.API
 import TahoeLAFS.Storage.Client
 import Text.Megaparsec
 
 main :: IO ()
 main = do
-    run
+    [storageFURLStr, capStr, shareNumStr] <- getArgs
+    let Right (CHKReader cap@Reader{verifier = Verifier{..}}) = parse pCapability "argv[2]" (Data.Text.pack capStr)
+        Just URI{uriAuthority = Just URIAuth{uriRegName = hostname, uriPort = (':' : port)}, uriPath = ('/' : swissnum)} = parseFURL storageFURLStr
 
-fixAccept :: Applicative f => Request -> f Request
-fixAccept req = pure req{requestHeaders = ("Authorization", "Tahoe-LAFS a2xwc2hmeTVqNmNyZzZnb3I0d2pyY2Fza3p0NzVncWQ=") : requestHeaders req}
+    run (Data.Text.unpack . Data.Text.toLower . encodeBase32Unpadded $ storageIndex) hostname (read port) swissnum (ShareNumber (read shareNumStr))
 
-fixAcceptPrint req = do
+parseFURL :: String -> Maybe URI
+parseFURL = parseURI . Data.Text.unpack . Data.Text.replace "tcp:" "" . Data.Text.pack
+
+fixAccept :: Applicative f => String -> Request -> f Request
+fixAccept swissnum req =
+    pure req{requestHeaders = ("Authorization", "Tahoe-LAFS " <> enc swissnum) : requestHeaders req}
+  where
+    enc = encodeUtf8 . encodeBase64 . encodeUtf8 . Data.Text.pack
+
+fixAcceptPrint swissnum req = do
     print req
-    pure req{requestHeaders = ("Authorization", "Tahoe-LAFS a2xwc2hmeTVqNmNyZzZnb3I0d2pyY2Fza3p0NzVncWQ=") : requestHeaders req}
+    fixAccept swissnum req
 
--- XXX turn this into a demo program that takes cmdline params to do this same thing
-
-run :: IO ()
-run = do
+run ::
+    -- | The base32-encoded storage index for which to request share info.
+    String ->
+    -- | The hostname or IP address of the storage server to query.
+    String ->
+    -- | The port number of the storage server to query.
+    Int ->
+    -- | The swissnum of the storage service
+    String ->
+    -- | A share number to download from the server.
+    ShareNumber ->
+    IO ()
+run storageIndex hostname port swissnum shareNum = do
     let tlsSettings = TLSSettingsSimple True True True
         sockSettings = Nothing
-        managerSettings = (mkManagerSettings tlsSettings sockSettings){managerModifyRequest = fixAcceptPrint}
+        managerSettings = (mkManagerSettings tlsSettings sockSettings){managerModifyRequest = fixAccept swissnum}
     manager' <- newTlsManagerWith managerSettings
     let manager'' = manager'
-        callIt = flip runClientM (mkClientEnv manager' (BaseUrl Https "localhost" 33337 ""))
-    print "getImmutableShareNumbers - succeeds!"
-    sharez <- callIt $ getImmutableShareNumbers "p6edaziufni6ajhw6x4zoq24ni"
+        callIt = flip runClientM (mkClientEnv manager' (BaseUrl Https hostname port ""))
+
+    putStrLn "getVersion"
+    ver <- callIt version
+    showIt ver
+    putStrLn "getImmutableShareNumbers:"
+    sharez <- callIt $ getImmutableShareNumbers storageIndex
     showIt sharez
-    print "readImmutableShares - succeeds! but returns a CHK with the wrong MIME type"
-    chk <- callIt $ readImmutableShares "p6edaziufni6ajhw6x4zoq24ni" (ShareNumber 0) Nothing -- (Just [ByteRangeFrom 1])
+    putStrLn "readImmutableShares - succeeds!"
+    chk <- callIt $ readImmutableShares storageIndex shareNum Nothing
     showIt chk
 
 flomp bip = dangerRealShow . CHKVerifier . verifier <$> parse pReader "" bip
@@ -80,6 +107,3 @@ swrite fname val = BSL.writeFile fname (serialise val)
 
 sread :: FilePath -> IO Version
 sread fname = deserialise <$> BSL.readFile fname
-
-tahoe :: BSL.ByteString
-tahoe = "\162X/http://allmydata.org/tahoe/protocols/storage/v1\163X\FSmaximum-immutable-share-size\ESC\NUL\NUL\NUL6+\167\230\NULX\SUBmaximum-mutable-share-size\ESC\NUL\245\130\161\161\&4\DLE\NULOavailable-space\ESC\NUL\NUL\NUL6+\167\230\NULSapplication-versionX\EMtahoe-lafs/1.18.0.post908"
