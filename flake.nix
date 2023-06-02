888f7b89b23d1beee466e6d8ce9d2a857676a67f
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-06-02 12:44:00 -0400

    get the flake in shape to work with the ci config

--- flake.nix
+++ flake.nix
@@ -17,8 +17,9 @@
     ulib = flake-utils.lib;
   in
     ulib.eachSystem ["x86_64-linux" "aarch64-darwin"] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
       hslib = hs-flake-utils.lib {
-        pkgs = nixpkgs.legacyPackages.${system};
+        inherit pkgs;
         src = ./.;
         compilerVersion = "ghc8107";
         packageName = "tahoe-capabilities";
@@ -27,5 +28,25 @@
       checks = hslib.checks {};
       devShells = hslib.devShells {};
       packages = hslib.packages {};
+
+      apps.hlint = hslib.apps.hlint {};
+
+      # Using the working directory of `nix run`, do a build with cabal and
+      # then run the test suite.
+      apps.cabal-test = {
+        type = "app";
+        program = "${
+          pkgs.writeShellApplication {
+            name = "cabal-build-and-test";
+            runtimeInputs = with pkgs; [pkg-config haskell.compiler.${ghcVersion} cabal-install];
+
+            text = ''
+              cabal update hackage.haskell.org
+              cabal build all
+              cabal run tests
+            '';
+          }
+        }/bin/cabal-build-and-test";
+      };
     });
 }
