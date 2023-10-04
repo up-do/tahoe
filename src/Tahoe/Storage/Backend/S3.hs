abe9637564db4efe3357d69176e2d6ead0669316
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-10-04 15:42:44 -0400

    a first step towards an S3Backend

--- src/Tahoe/Storage/Backend/S3.hs
+++ src/Tahoe/Storage/Backend/S3.hs
@@ -1,4 +1,15 @@
-module Tahoe.Storage.Backend.S3 (someFunc) where
+module Tahoe.Storage.Backend.S3 where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Amazonka (runResourceT)
+import qualified Amazonka as AWS
+import qualified Amazonka.S3 as S3
+import TahoeLAFS.Storage.API (AllocateBuckets (..), AllocationResult (..))
+import TahoeLAFS.Storage.Backend (Backend (..))
+
+data S3Backend = S3Backend AWS.Env S3.BucketName
+
+instance Backend S3Backend where
+    createImmutableStorageIndex (S3Backend env name) storageIndex (AllocateBuckets{shareNumbers, allocatedSize}) =
+        runResourceT $ do
+            objects <- AWS.send env (S3.newListObjectsV2 name)
+            pure $ AllocationResult{alreadyHave = [], allocated = []}
