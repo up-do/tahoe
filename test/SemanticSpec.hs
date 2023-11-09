8f338b67df0b5928c1050df948131d00f760b0c6
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2023-11-09 16:24:19 -0500

    Memory backend passes the external test suite

--- test/SemanticSpec.hs
+++ test/SemanticSpec.hs
@@ -1,70 +1,37 @@
-{-# LANGUAGE FlexibleInstances #-}
-
 module SemanticSpec (
     spec,
 ) where
-
-import Prelude hiding (
-    lookup,
-    toInteger,
- )
 
 import Control.Monad (
     void,
     when,
  )
-
 import Data.Bits (
     xor,
  )
-
+import qualified Data.ByteString as B
+import Data.Data (Proxy (Proxy))
+import Data.IORef (IORef)
+import Data.Interval (Boundary (Closed, Open), Extended (Finite), Interval, interval, lowerBound, upperBound)
+import qualified Data.IntervalSet as IS
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
 import GHC.Word (
     Word8,
  )
-
-import qualified Data.Map.Strict as Map
-import qualified Data.Set as Set
-
+import Network.HTTP.Types (ByteRange (ByteRangeSuffix))
 import System.Directory (
     removeDirectoryRecursive,
  )
-
 import System.IO.Temp (
     createTempDirectory,
     getCanonicalTemporaryDirectory,
  )
-
-import Test.Hspec (
-    Spec,
-    context,
-    describe,
-    it,
-    shouldBe,
-    shouldThrow,
+import Tahoe.Storage.Testing.Spec (
+    ShareNumbers (..),
+    genStorageIndex,
+    makeStorageSpec,
  )
-import Test.QuickCheck (
-    Gen,
-    NonEmptyList (getNonEmpty),
-    NonNegative (NonNegative),
-    Positive (..),
-    Property,
-    chooseInteger,
-    counterexample,
-    forAll,
-    ioProperty,
-    oneof,
-    property,
-    vector,
-    (==>),
- )
-
-import Test.QuickCheck.Monadic (
-    monadicIO,
-    run,
- )
-
-import qualified Data.ByteString as B
-
 import TahoeLAFS.Storage.API (
     AllocateBuckets (AllocateBuckets),
     AllocationResult (AllocationResult),
@@ -88,7 +55,6 @@ import TahoeLAFS.Storage.API (
     toInteger,
     writev,
  )
-
 import TahoeLAFS.Storage.Backend (
     Backend (
         abortImmutableUpload,
@@ -102,29 +68,52 @@ import TahoeLAFS.Storage.Backend (
     WriteImmutableError (..),
     writeMutableShare,
  )
-
-import Data.IORef (IORef)
-
-import TahoeLAFS.Storage.Backend.Memory (
-    MemoryBackend (..),
-    MutableShareSize (MutableShareSize),
-    memoryBackend,
-    shareDataSize,
-    toMutableShareSize,
- )
-
-import Data.Data (Proxy (Proxy))
-import Data.Interval (Boundary (Closed, Open), Extended (Finite), Interval, interval, lowerBound, upperBound)
-import qualified Data.IntervalSet as IS
-import Tahoe.Storage.Testing.Spec (
-    ShareNumbers (..),
-    genStorageIndex,
-    makeStorageSpec,
- )
 import TahoeLAFS.Storage.Backend.Filesystem (
     FilesystemBackend (FilesystemBackend),
  )
+import TahoeLAFS.Storage.Backend.Memory (
+    MemoryBackend (..),
+    MutableShareSize (..),
+    SecretProtected (..),
+    addShares,
+    memoryBackend,
+    queryRangeToReadVector,
+    readvToQueryRange,
+    shareDataSize,
+    toMutableShareSize,
+ )
+import Test.Hspec (
+    Spec,
+    context,
+    describe,
+    it,
+    shouldBe,
+    shouldThrow,
+ )
+import Test.QuickCheck (
+    Gen,
+    NonEmptyList (getNonEmpty),
+    NonNegative (NonNegative),
+    Positive (..),
+    Property,
+    chooseInteger,
+    counterexample,
+    forAll,
+    ioProperty,
+    oneof,
+    property,
+    vector,
+    (==>),
+ )
 import Test.QuickCheck.Classes (Laws (..), semigroupMonoidLaws)
+import Test.QuickCheck.Monadic (
+    monadicIO,
+    run,
+ )
+import Prelude hiding (
+    lookup,
+    toInteger,
+ )
 
 -- | Create a Spec that checks the given Laws.
 lawsCheck :: Laws -> Spec
@@ -137,7 +126,7 @@ lawsCheck Laws{lawsTypeclass, lawsProperties} =
 spec :: Spec
 spec = do
     context "utilities" $ do
-        describe "MutableShareStorage" $ do
+        describe "toMutableShareSize" $ do
             it "finds the larger size for some cases" $ do
                 toMutableShareSize (WriteVector 0 "x") <> toMutableShareSize (WriteVector 1 "x")
                     `shouldBe` MutableShareSize 0 2
@@ -150,6 +139,26 @@ spec = do
 
                 toMutableShareSize (WriteVector 0 "Hello") <> toMutableShareSize (WriteVector 3 "world")
                     `shouldBe` MutableShareSize 0 8
+
+        describe "addShares" $ do
+            it "prepends the new write to the share storage" $ do
+                let si = "storageindex"
+                    enabler = WriteEnablerSecret "enabler"
+                    addShares' = addShares si enabler
+                    shareNum = ShareNumber 0
+                    Just a = addShares' mempty (Map.fromList [(shareNum, [WriteVector 1 "first"])])
+                    Just b = addShares' a (Map.fromList [(shareNum, [WriteVector 2 "second"])])
+
+                b `shouldBe` Map.fromList [(si, SecretProtected enabler (Map.fromList [(shareNum, [WriteVector 2 "second", WriteVector 1 "first"])]))]
+
+            it "puts later elements in a single WriteVector list earlier in the MutableShareStorage list" $ do
+                let si = "storageindex"
+                    enabler = WriteEnablerSecret "enabler"
+                    addShares' = addShares si enabler
+                    shareNum = ShareNumber 0
+                    Just a = addShares' mempty (Map.fromList [(shareNum, [WriteVector 1 "first", WriteVector 2 "second"])])
+
+                a `shouldBe` Map.fromList [(si, SecretProtected enabler (Map.fromList [(shareNum, [WriteVector 2 "second", WriteVector 1 "first"])]))]
 
         describe "shareDataSize" $ do
             it "converts list of WriteVector to a size" $ do
@@ -171,6 +180,14 @@ spec = do
             . lawsCheck
             . semigroupMonoidLaws
             $ (Proxy :: Proxy ReadTestWriteVectors)
+
+        describe "ReadVector" $ do
+            it "it round-trips through queryRangeToReadVector / readvToQueryRange" $
+                property $ \rvs ->
+                    (queryRangeToReadVector 1234 . readvToQueryRange) rvs `shouldBe` rvs
+
+            it "imposes a lower bound of zero on offset" $ do
+                queryRangeToReadVector 1 (Just [ByteRangeSuffix 2]) `shouldBe` [ReadVector 0 1]
 
     context "memory" $ makeStorageSpec memoryBackend cleanupMemory
     context "filesystem" $ makeStorageSpec filesystemBackend cleanupFilesystem
