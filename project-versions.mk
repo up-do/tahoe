a586076b0fb8bd341857ff7faf38ef6af9bb7d12
Phil de Joux <philderbeast@gmail.com>
2024-09-30 11:34:45 -0400

    Configure updo for ghc-9.6.6

--- project-versions.mk
+++ project-versions.mk
@@ -4,8 +4,8 @@ GHC_VERSION ?= 8.10.7
 STACKAGE_VERSION ?= lts-18.28
 
 # For the upgrade, pick a matching pair of ghc-version and stack resolver.
-GHC_UPGRADE ?= 8.10.7
-STACKAGE_UPGRADE ?= lts-18.28
+GHC_UPGRADE ?= 9.6.6
+STACKAGE_UPGRADE ?= lts-22.36
 
 # Imports can be relative to the project or relative to importing file.
 # ImportRelative works with cabal-3.10 and is the default.
