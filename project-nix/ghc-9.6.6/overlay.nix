f5e9c682b2a6aea3121602e4ff8ffa1d3a8bf046
Phil de Joux <philderbeast@gmail.com>
2024-10-04 16:43:19 -0400

    Fixup ghc version and relative paths

--- project-nix/ghc-9.6.6/overlay.nix
+++ project-nix/ghc-9.6.6/overlay.nix
@@ -1,5 +1,5 @@
   self: super: {
-    ghcVersion = "ghc963";
+    ghcVersion = "ghc966";
 
     all-cabal-hashes =
         # Update revision to match required hackage
@@ -8,13 +8,41 @@
           sha256 = "sha256-qJt8ZAG/Ilx0JMyd/FTuHB7k5SpYqkLJFUCi3uEBhvU=";
     };
 
-    haskellPackages = super.haskell.packages.${self.ghcVersion}.override {
-      overrides = haskellSelf: haskellSuper: {
-        crypton = self.haskell.lib.dontCheck (haskellSuper.callHackage "crypton" "1.0.0" {});
-        fec = self.haskell.lib.dontCheck (haskellSuper.callHackage "fec" "0.2.0" {});
-        network-simple-tls = self.haskell.lib.dontCheck (haskellSuper.callHackage "network-simple-tls" "0.4.2" {});
-        servant-js = self.haskell.lib.dontCheck (haskellSuper.callHackage "servant-js" "0.9.4.2" {});
-        stm-lifted = self.haskell.lib.dontCheck (haskellSuper.callHackage "stm-lifted" "2.5.0.0" {});
-      };
-    };
+    # haskellPackages = super.haskell.packages.${self.ghcVersion}.override {
+    #   overrides = haskellSelf: haskellSuper: {
+    #     crypton = haskellSuper.callHackage "crypton" "1.0.0" {};
+    #     fec = haskellSuper.callHackage "fec" "0.2.0" {};
+    #     network-simple-tls = haskellSuper.callHackage "network-simple-tls" "0.4.2" {};
+    #     servant-js = haskellSuper.callHackage "servant-js" "0.9.4.2" {};
+    #     stm-lifted = haskellSuper.callHackage "stm-lifted" "2.5.0.0" {};
+    # };
+
+    haskellPackages = super.haskellPackages.override (old: {
+      overrides =
+        let
+          nlib = self.lib;
+          hlib = self.haskell.lib;
+
+          spkgs =
+              hlib.packageSourceOverrides {
+                tahoe-s3 = ./../../s3;
+                tahoe-capabilities = ./../../capabilities;
+                tahoe-great-black-swamp-types = ./../../swamp/types;
+              };
+
+          dpkgs = 
+              hlib.packagesFromDirectory {
+                directory = ./packages;
+              };
+
+          wpkgs = x: _:
+            {
+              tahoe-great-black-swamp = x.callCabal2nixWithOptions "tahoe-great-black-swamp" ./../../swamp/swamp "--no-check" {};
+              tahoe-great-black-swamp-testing = x.callCabal2nix "tahoe-great-black-swamp-testing" ./../../swamp/testing {};
+            };
+        in
+          # nlib.fold nlib.composeExtensions (old.overrides or (_: _: { })) [ spkgs dpkgs ];
+          # nlib.composeManyExtensions (old.overrides or (_: _: { })) [ spkgs dpkgs ];
+          nlib.composeManyExtensions [ spkgs dpkgs wpkgs ];
+    });
   }
